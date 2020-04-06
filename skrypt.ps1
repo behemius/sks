8296f81a464df29e4ae76af7b684b1a1c10c5d1f
Krzysztof Wroblewski <behemius@gmail.com>
2020-04-06 21:04:39 +0200

    Adding SML condition

--- skrypt.ps1
+++ skrypt.ps1
@@ -1,1 +1,12 @@
 echo "Hello World!"
+$size = Read-Host -Prompt "Provide S/M/L parameter"
+
+if ($size -eq "S") {
+     Write-Host "Small"
+} elseif ($size -eq "M") {
+     Write-Host "Medium"
+} elseif ($size -eq "L") {
+	 Write-Host "Large"
+} else {
+     Write-Host "Bzdet"
+}
